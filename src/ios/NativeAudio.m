121ddcc997ae46c2044d1fc09fb4d5d944a40f79
Martin DONADIEU <martindonadieu@gmail.com>
2020-06-21 16:17:44 +0200

    allow plauy music in background

--- src/ios/NativeAudio.m
+++ src/ios/NativeAudio.m
@@ -45,6 +45,7 @@ NSString* INFO_VOLUME_CHANGED = @"(NATIVE AUDIO) Volume changed.";
     }
 
     [session setActive: YES error: nil];
+    [session setCategory:AVAudioSessionCategoryAmbient error:nil];
     //[session setCategory:AVAudioSessionCategoryPlayback error:nil];
 }
 
